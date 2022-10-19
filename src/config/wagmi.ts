c4d1709df8be6d617c1ef33382a4200d261b98cd
OleanjiKingCode <holamilean56@gmail.com>
2022-10-19 20:54:57 +0100

    wallet connect

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -2,14 +2,13 @@ import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { chain, configureChains } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from 'wagmi/providers/public'
 import { config } from "./index";
 
 type Connector = MetaMaskConnector | WalletConnectConnector;
 const chainArray =
-  config.alchemyChain === "goerli" ? [chain.goerli] : [chain.mainnet];
-export const { chains, provider } = configureChains(chainArray, [
-  alchemyProvider({ alchemyId: config.alchemyApiKey, weight: 1 }),
-]);
+  config.alchemyChain === "mumbai" ? [chain.polygonMumbai] : [chain.polygon];
+export const { chains, provider } = configureChains(chainArray, [publicProvider()],);
 
 export const connectors: Connector[] = [
   new MetaMaskConnector({ chains, options: { shimDisconnect: true } }),
