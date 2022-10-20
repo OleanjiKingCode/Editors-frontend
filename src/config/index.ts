f4ac9c574909862561ac0235c444534f05595559
OleanjiKingCode <holamilean56@gmail.com>
2022-10-20 12:05:17 +0100

    envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,25 @@
 const testnet = true;
 export const config = {
-  alchemyChain: testnet ? "mumbai" : "polygon",
+  alchemyChain: testnet
+    ? String(process.env.TEST_ALCHEMY_CHAIN)
+    : String(process.env.MAIN_ALCHEMY_CHAIN),
   isTestnet: testnet,
-  chainId: testnet ? 80001 : 137,
-  chainName: testnet ? "mumbai" : "Polygon Mainnet",
+  chainId: testnet ? process.env.TEST_CHAIN_ID : process.env.MAIN_CHAIN_ID,
+  chainName: testnet
+    ? String(process.env.TEST_CHAIN_NAME)
+    : String(process.env.MAIN_CHAIN_NAME),
   payoutsContractAddress: testnet
     ? process.env.TEST_PAYOUTS_CONTRACT_ADDRERSS ||
       "0xCca2C72a79e4F3307caa469F12085Be6Fca2E15f"
     : process.env.MAIN_PAYOUTS_CONTRACT_ADDRERSS ||
       "0x24d6D640D36F45D65F289774B986a6c61C95E02F",
-  payoutsGraphApi: testnet ?
-    process.env.TEST_PAYOUTS_GRAPH_API ||
-    "https://api.thegraph.com/subgraphs/name/oleanjikingcode/payout" :process.env.MAIN_PAYOUTS_GRAPH_API ||
-    "https://thegraph.com/hosted-service/subgraph/oleanjikingcode/editors-payout",
+  payoutsGraphApi: testnet
+    ? process.env.TEST_PAYOUTS_GRAPH_API ||
+      "https://api.thegraph.com/subgraphs/name/oleanjikingcode/payout"
+    : process.env.MAIN_PAYOUTS_GRAPH_API ||
+      "https://thegraph.com/hosted-service/subgraph/oleanjikingcode/editors-payout",
   iqPolygonAddress: "0xB9638272aD6998708de56BBC0A290a1dE534a578",
-  Scan: testnet ? "https://mumbai.polygonscan.com/" : "https://polygonscan.com",
+  Scan: testnet
+    ? String(process.env.TEST_SCAN_LINK)
+    : String(process.env.MAIN_SCAN_LINK),
 };
