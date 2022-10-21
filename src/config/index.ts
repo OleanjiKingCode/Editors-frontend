743e8a1c5c6201194556a72152228488153292c5
OleanjiKingCode <holamilean56@gmail.com>
2022-10-21 09:48:22 +0100

    .envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,14 @@
 export const config = {
-  alchemyChain: String(process.env.TEST_ALCHEMY_CHAIN),
+  alchemyChain: String(process.env.ALCHEMY_CHAIN),
   isTestnet: true,
-  chainId: process.env.TEST_CHAIN_ID,
-  chainName: String(process.env.MAIN_CHAIN_NAME),
+  chainId: process.env.CHAIN_ID,
+  chainName: String(process.env.CHAIN_NAME),
   payoutsContractAddress:
-    process.env.TEST_PAYOUTS_CONTRACT_ADDRERSS ||
+    process.env.PAYOUTS_CONTRACT_ADDRERSS ||
     "0xCca2C72a79e4F3307caa469F12085Be6Fca2E15f",
   payoutsGraphApi:
-    process.env.TEST_PAYOUTS_GRAPH_API ||
+    process.env.PAYOUTS_GRAPH_API ||
     "https://api.thegraph.com/subgraphs/name/oleanjikingcode/payout",
   iqPolygonAddress: "0xB9638272aD6998708de56BBC0A290a1dE534a578",
-  Scan: String(process.env.TEST_SCAN_LINK),
+  Scan: String(process.env.SCAN_LINK),
 };
